90ed9f2959d8be4c61d67e679b0720f4885789fd
Cdm2883 <Cdm2883@hotmail.com>
2024-10-16 22:49:19 +0800

    build: from gradle-multi-module-project-template

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+rootProject.name = "gradle-multi-module-project-template"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage")
+    repositories {
+        mavenCentral()
+    }
+}
+
+//include(":")
