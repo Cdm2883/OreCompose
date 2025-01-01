12ce967fa79efd28005fdbd6a6b9983cd12e70fe
Cdm2883 <Cdm2883@hotmail.com>
2025-01-01 21:18:46 +0800

    docs: panorama equirectangular generation method

--- oreui-panorama/src/commonMain/kotlin/vip/cdms/orecompose/layout/panorama/Panorama.kt
+++ oreui-panorama/src/commonMain/kotlin/vip/cdms/orecompose/layout/panorama/Panorama.kt
@@ -23,6 +23,45 @@ fun Modifier.foreground(color: Color) = drawWithContent {
     drawRect(color)
 }
 
+/**
+ * ```python
+ * import py360convert
+ * import cv2
+ *
+ * resource_packs = "<PATH_TO_YOURS>/textures/ui/"
+ * out_path = "<PATH_TO_YOURS>/panorama.png"
+ * quality_para = 0
+ * target_size_w = 3000
+ *
+ * cube_dice0 = cv2.imread(f"{resource_packs}panorama_0.png")
+ * cube_dice1 = cv2.imread(f"{resource_packs}panorama_1.png")
+ * cube_dice2 = cv2.imread(f"{resource_packs}panorama_2.png")
+ * cube_dice3 = cv2.imread(f"{resource_packs}panorama_3.png")
+ * cube_dice4 = cv2.imread(f"{resource_packs}panorama_4.png")
+ * cube_dice5 = cv2.imread(f"{resource_packs}panorama_5.png")
+ *
+ * target_size_w = int(target_size_w)
+ * target_size_h = int(target_size_w // 2)
+ * quality_map = {
+ *     0: 40,
+ *     1: 60,
+ *     2: 90,
+ *     3: 100
+ * }
+ * quality_save = quality_map.get(quality_para, 70)
+ * cube_dice1 = cv2.flip(cube_dice1, 1)
+ * cube_dice2 = cv2.flip(cube_dice2, 1)
+ * cube_dice4 = cv2.flip(cube_dice4, 0)
+ *
+ * res = py360convert.c2e(
+ *     [cube_dice0, cube_dice1, cube_dice2, cube_dice3, cube_dice4, cube_dice5],
+ *     target_size_h, target_size_w, cube_format='list'
+ * )
+ * cv2.imwrite(out_path, res, [int(cv2.IMWRITE_JPEG_QUALITY), quality_save])
+ * print("Panorama image saved at: " + out_path)
+ * ```
+ */
+@Suppress("SpellCheckingInspection")
 @Composable
 expect fun Panorama(
     equirectangular: ImageBitmap = imageResource(PanoramaDefaults.Equirectangular),
