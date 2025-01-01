729e5d0a152acab1cb56243e1dba7fe263194bc5
Cdm2883 <Cdm2883@hotmail.com>
2025-01-02 01:14:47 +0800

    feat(gallery/android): enable edge to edge

--- gallery/src/androidMain/kotlin/vip/cdms/orecompose/gallery/MainActivity.kt
+++ gallery/src/androidMain/kotlin/vip/cdms/orecompose/gallery/MainActivity.kt
@@ -3,11 +3,13 @@ package vip.cdms.orecompose.gallery
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        enableEdgeToEdge()
         setContent {
             App()
         }
