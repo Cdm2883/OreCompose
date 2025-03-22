112dac1c7fbe14cd586e5bd3ad6ca901e89a726e
Cdm2883 <Cdm2883@hotmail.com>
2025-03-22 23:16:51 +0800

    refactor!: remove composable annotation for accessibility indicator

--- oreui/src/commonMain/kotlin/vip/cdms/orecompose/utils/Accessibility.kt
+++ oreui/src/commonMain/kotlin/vip/cdms/orecompose/utils/Accessibility.kt
@@ -5,29 +5,43 @@ import androidx.compose.foundation.interaction.collectIsFocusedAsState
 import androidx.compose.foundation.interaction.collectIsHoveredAsState
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.input.key.key
 import androidx.compose.ui.input.key.onKeyEvent
 import androidx.compose.ui.zIndex
+import vip.cdms.orecompose.effect.LocalOutlineWidth
 import vip.cdms.orecompose.effect.ZIndices
+import vip.cdms.orecompose.effect.accInnerOutlinesWidth
 import vip.cdms.orecompose.effect.outline
 import vip.cdms.orecompose.style.OreColors
 
-val LocalAccessibilityIndicatorColor = staticCompositionLocalOf { OreColors.PureWhite }
+val LocalAccessibilityIndicatorColor = staticCompositionLocalOf<Color?> { OreColors.PureWhite }
 
-@Composable
-fun Modifier.accessibilityIndicator(interactionSource: InteractionSource) = run {
-    var isTab by remember { mutableStateOf(false) }
-    val focused = interactionSource.collectIsFocusedAsState().value
-    val hovered = interactionSource.collectIsHoveredAsState().value
-    remember(hovered) { isTab = false }
+fun Modifier.accessibilityIndicator(interactionSource: InteractionSource) = accInnerOutlinesWidth().let { padding ->
+    // WARNING: using `composed` will cause all the padding nodes after this node
+    // calculated a wrong padding (ignore this). Because foreach modifier chain in this case
+    // will be a ComposedModifier rather than OutlineElement. We cannot get the element inside
+    // before it actually drawing.
+    //
+    // But for accessibility indicator, maybe it's not a big problem...
+    // Because the indicator usually be the last element of modifiers.
+    composed {
+        val color = LocalAccessibilityIndicatorColor.current ?: return@composed this
 
-    onKeyEvent {
-        isTab = it.key == Key.Tab
-        false
-    }.run {
-        if (isTab && focused && !hovered) zIndex(ZIndices.ACCESSIBILITY_INDICATOR)
-            .outline(color = LocalAccessibilityIndicatorColor.current, padding = false)
-        else this
+        var isTab by remember { mutableStateOf(false) }
+        val focused = interactionSource.collectIsFocusedAsState().value
+        val hovered = interactionSource.collectIsHoveredAsState().value
+        remember(hovered) { isTab = false }
+
+        onKeyEvent {
+            isTab = it.key == Key.Tab
+            false
+        }.run {
+            if (isTab && focused && !hovered) zIndex(ZIndices.ACCESSIBILITY_INDICATOR)
+                .outline(color, LocalOutlineWidth.current.toDp(), padding)
+            else this
+        }
     }
 }
