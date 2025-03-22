abddb06316b8c8ded6213959c876062ee838ddc6
Cdm2883 <Cdm2883@hotmail.com>
2025-03-22 22:52:06 +0800

    fix: correcter accessibility indicator behavior for touching

--- oreui/src/commonMain/kotlin/vip/cdms/orecompose/utils/Accessibility.kt
+++ oreui/src/commonMain/kotlin/vip/cdms/orecompose/utils/Accessibility.kt
@@ -5,6 +5,9 @@ import androidx.compose.foundation.interaction.collectIsFocusedAsState
 import androidx.compose.foundation.interaction.collectIsHoveredAsState
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.key
+import androidx.compose.ui.input.key.onKeyEvent
 import androidx.compose.ui.zIndex
 import vip.cdms.orecompose.effect.ZIndices
 import vip.cdms.orecompose.effect.outline
@@ -14,15 +17,17 @@ val LocalAccessibilityIndicatorColor = staticCompositionLocalOf { OreColors.Pure
 
 @Composable
 fun Modifier.accessibilityIndicator(interactionSource: InteractionSource) = run {
-    var isTab by remember { mutableStateOf(true) }
-
+    var isTab by remember { mutableStateOf(false) }
     val focused = interactionSource.collectIsFocusedAsState().value
-    remember(focused) { isTab = true }
-
     val hovered = interactionSource.collectIsHoveredAsState().value
     remember(hovered) { isTab = false }
 
-    if (isTab && focused && !hovered) zIndex(ZIndices.ACCESSIBILITY_INDICATOR)
-        .outline(color = LocalAccessibilityIndicatorColor.current, padding = false)
-    else this
+    onKeyEvent {
+        isTab = it.key == Key.Tab
+        false
+    }.run {
+        if (isTab && focused && !hovered) zIndex(ZIndices.ACCESSIBILITY_INDICATOR)
+            .outline(color = LocalAccessibilityIndicatorColor.current, padding = false)
+        else this
+    }
 }
